497f966a7eb357b94384c64a286f73a71460aa08
Ericson Balajadia <22-1-01630@vsu.edu.ph>
2025-09-21 09:01:56 +0800

    fix: improves responsiveness of game board
    Enhances the game's responsiveness across various screen sizes, especially on smaller devices.
    
    This includes:
    - Reduced gaps and margins for better layout on tablets and phones.
    - Smaller square sizes for very small phones to fit the screen.
    - Centers the board and squares for consistent display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import Game from './Components/Game';
 
 function App() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handlePlay(nextSquares: (string | null)[]) {
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
+  
 
   return (
     <div className="app">
